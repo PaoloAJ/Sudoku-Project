504bfc4748b6f565d53d07cde67b4aed2b04abfd
Alvin Cabe <jackcabe123@gmail.com>
2024-11-18 16:14:03 -0500

    mmonkey

--- Frontend/cell.py
+++ Frontend/cell.py
@@ -1,2 +1,16 @@
 class Cell:
-    None
+    def __init__(self, value, row, col, screen):
+        self.value = value
+        self.row = row
+        self.col = col
+        self.screen = screen
+        self.sketched_value = 0
+
+    def set_cell_value(self,value):
+        self.value = value
+
+    def set_sketched_value(self, value):
+        self.sketched_value = value
+
+    def draw(self):
+        pass
